2d8f03a460c3fbc4263bdcd95c61f69a2157e484
akopton <kopton.olek@gmail.com>
2022-07-12 21:19:35 +0200

    first styles, defined calculator methods

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-const result = document.querySelector('.calculator__result');
-const buttons = document.querySelectorAll('.button');
-const calcBtns = [];
+const display = document.querySelector('.calculator__result');
+const valueHandler = document.querySelector('.display__value-handler');
+const valueButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]:not([data-operation=equal])');
+const clearAllButton = document.querySelector('[data-clear-all]');
+const deleteButton = document.querySelector('[data-delete]');
+const equalsButton = document.querySelector('[data-operation=equal]')
 
+class Calculator {
+    constructor(firstOperand, secondOperand) {
+        this.firstOperand = firstOperand
+        this.secondOperand = secondOperand
+        this.clear()
+    }
 
-function setButtons() {
-    buttons.forEach(btn => {
-        calcBtns.push({
-            id: btn.id,
-            element: btn,
-            operation: btn.value ? '' : btn.innerHTML,
-            value: btn.value ? btn.value : ''
-        })
-    })
+    clear() {
+
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+
+    }
 }
-setButtons();
-
-
-
-
-
-
-
