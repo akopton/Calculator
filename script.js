c9bf01847f315b29b722da91aa82ee497d790edb
akopton <kopton.olek@gmail.com>
2022-07-12 21:53:19 +0200

    implemented number showing on display

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const display = document.querySelector('.calculator__result');
-const valueHandler = document.querySelector('.display__value-handler');
+const currentOperandElement = document.querySelector('.calculator__result'); // value handler (main display)
+const previousOperandElement = document.querySelector('.display__value-handler'); // value handler (small display)
 const valueButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]:not([data-operation=equal])');
 const clearAllButton = document.querySelector('[data-clear-all]');
@@ -7,14 +7,16 @@ const deleteButton = document.querySelector('[data-delete]');
 const equalsButton = document.querySelector('[data-operation=equal]')
 
 class Calculator {
-    constructor(firstOperand, secondOperand) {
-        this.firstOperand = firstOperand
-        this.secondOperand = secondOperand
+    constructor(previousOperandElement, currentOperandElement) {
+        this.previousOperandElement = previousOperandElement
+        this.currentOperandElement = currentOperandElement
         this.clear()
     }
 
     clear() {
-
+        this.previousOperand = ''
+        this.currentOperand = ''
+        this.operation = undefined
     }
 
     delete() {
@@ -22,7 +24,8 @@ class Calculator {
     }
 
     appendNumber(number) {
-
+        if (number === '.' && this.currentOperandElement.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
     }
 
     chooseOperation(operation) {
@@ -34,6 +37,22 @@ class Calculator {
     }
 
     updateDisplay() {
-
+        this.currentOperandElement.innerText = this.currentOperand
     }
 }
+
+const calculator = new Calculator(previousOperandElement, currentOperandElement);
+
+valueButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        calculator.appendNumber(btn.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+operationButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        calculator.chooseOperation(btn.innerText);
+        calculator.updateDisplay();
+    })
+})
